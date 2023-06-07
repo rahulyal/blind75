7301c398ce6c784dbfac5b0e4cbcd72d7856699d
rahulyal <vyalavarthi@scu.edu>
2023-06-06 23:09:37 -0700

    1. Two Sum
    template and first problem with basic approach done

---
+++ Problems/1. Two Sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        i = 0
+        for num1 in nums:
+            j = i+1    
+            for num2 in nums[j:]:
+                if num1 + num2 == target:
+                    return [i,j]
+                j += 1
+            i += 1
+        return None
---
+++ Problems/problem_template.md
@@ -0,0 +1,24 @@
+# Problem Name
+
+leetcode problem link
+
+---
+
+Read and understand the problem
+
+---
+
+Approach towards the solution you want to code
+
+---
+
+Check if there is a better solution
+
+Write down the approach, and code that solution
+
+---
+
+Repeat approach and coding the solution, like REPs
+
+---
+
